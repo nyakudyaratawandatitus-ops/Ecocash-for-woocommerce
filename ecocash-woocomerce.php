0d76495a98a996c3e429c501e10f51d4ddba381f
nyakudyaratawandatitus-ops <nyakudyaratawandatitus@gmail.com>
2025-12-09 19:00:20 +0200

    Update ecocash-woocomerce.php

--- ecocash-woocomerce.php
+++ ecocash-woocomerce.php
@@ -1,1 +1,11 @@
+<?php
+/**
+ * Plugin Name: EcoCash gateway for WooCommerce
+ * Description: Accept EcoCash payments in WooCommerce with real-time processing.
+ * Version: 1.0.0
+ * Author: T.Titus
+  * Author URI: https://zimwebtech.co.zw
+ * Plugin URI: https://eduzim.co.zw/mutoko-connect-wifi/product/test/
+ */
 
+if (!defined('ABSPATH')) exit;
